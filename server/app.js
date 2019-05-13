977a83f26c8c2e93a5f91fb207449ebae2f1a20b
mrfong <therealshadix@gmail.com>
2019-05-13 12:07:44 +0300

    imported compression, disabled powered by, installed compression into package.json relates to #42

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const compression = require('compression');
 const controllers = require('./controllers/routes');
 
 const app = express();
 
+app.disable('x-powered-by');
+app.use(compression());
 app.use(controllers);
 app.set('port', process.env.PORT || 7777);
 

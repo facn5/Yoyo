4cbebbd5e86717ad1dce7d7fa9abbc365d5d141b
faris114 <farisabutaya@gmail.com>
2019-05-13 17:17:48 +0300

    inital modification forward React router

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import SignupLogin from './landing-page/landing-page'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import AppRouter from './routes.js';
 
 class App extends Component {
   render() {
     return (
-      <div><h1>React and Webpack/Babel setup</h1></div>
+      <div>
+
+   <h1>React and Webpack/Babel setup</h1>
+      <SignupLogin />
+      <AppRouter />
+</div>
     )
   }
 }

cd2c263be6935bfa4724aa0a7255f60f2182e700
mrfong <therealshadix@gmail.com>
2019-05-13 12:18:50 +0300

    added a post routing point for /create-user relates to #42

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -7,5 +7,13 @@ routers.get('/', (req, res) => {
   res.send('main page layout');
 });
 
+routers.post('/create-user', (req, res) => {
+  // check the database if user exists
+
+  // create new entry in database with parent's info
+
+  // direct them to /parent-sign-in, and direct to /parent-profile
+  res.redirect('/parent-profile');
+});
 
 module.exports = routers;

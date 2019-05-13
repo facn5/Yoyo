47c321d9a91b6c952de9bb1bc7cf76258cf2f82c
faris114 <farisabutaya@gmail.com>
2019-05-13 17:23:58 +0300

    initial buttons landing page

---
+++ client/src/components/landing-page/landing-page.js
@@ -0,0 +1,13 @@
+import React from 'react' ;
+
+export default class SignupLogin extends React.Component {
+  render() {
+    return (
+      <div>
+      <button type="button"> Sign up </button>
+      <button type="button"> Login  </button>
+      </div>
+
+     )
+  }
+}

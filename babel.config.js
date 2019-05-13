0fab9544792608ba1c26b509facec83bd8a98aa7
misterrodger <johndavidrodger@gmail.com>
2019-05-13 16:15:12 +0300

    Update babel config to allow React state
    -Recent versions of Babel require additional plugin, and
    .babelrc to be renamed babel.config.js, with presets
    and plugins exported in that file
    -Babel changed the scope of the earlier .babelrc syntax,
    so different syntax is required for global scope
    -Relates #49

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [ "@babel/preset-env", "@babel/preset-react"],
+  plugins: [ "@babel/plugin-proposal-class-properties"]
+}
+
+// {"presets": ["@babel/preset-env", "@babel/preset-react"],
+// "plugins": ["@babel/plugin-proposal-class-properties"]}
+//
+// module.exports = {
+//   presets: [ "@babel/preset-env", "@babel/preset-react" ],
+//   plugins: [ "@babel/plugin-transform-arrow-functions", "@babel/plugin-proposal-class-properties" ]
+// }

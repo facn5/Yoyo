7188e3c9cea42db05fac0cf9c3c6f7e2cbcd27da
mrfong <therealshadix@gmail.com>
2019-05-13 12:21:00 +0300

    added a GET routing point for parent profile relates to #42

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -16,4 +16,20 @@ routers.post('/create-user', (req, res) => {
   res.redirect('/parent-profile');
 });
 
+routers.post('/create-user', (req, res) => {
+  // check the database if user exists
+
+  // create new entry in database with parent's info
+
+  // direct them to /parent-sign-in, and direct to /parent-profile
+  res.redirect('/parent-profile');
+});
+
+routers.get('parent-profile', (req, res) => {
+  // call the function that gets personal info and booked events from the parent's profile
+
+  res.redirect('/');
+});
+
+
 module.exports = routers;

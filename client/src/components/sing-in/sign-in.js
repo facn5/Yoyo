4d664851b0d6c9eceadb820b1d6647be3edcd6b6
faris114 <farisabutaya@gmail.com>
2019-05-13 17:26:35 +0300

    sign in added to test routing Relates #51

---
+++ client/src/components/sing-in/sign-in.js
@@ -0,0 +1,14 @@
+import React from 'react' ;
+
+export default class SignupLogin extends React.Component {
+  render() {
+    return (
+      <div>
+      <form>
+      <h1>hello</h1>
+      </form>
+      </div>
+
+     )
+  }
+}

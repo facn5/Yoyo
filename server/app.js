7f17419440f37d81454cf55e8dc430e21b4d44bf
mrfong <therealshadix@gmail.com>
2019-05-13 17:42:31 +0300

    removed the bodyParser, replaced with a built in functionality relates to #42

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const compression = require('compression');
-const bodyParser = require('body-parser');
 const controllers = require('./controllers/routes');
 
 const app = express();
 
 app.disable('x-powered-by');
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(compression());
 app.use(controllers);
 app.set('port', process.env.PORT || 7777);

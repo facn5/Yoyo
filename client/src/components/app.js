4d323a62e11d3d589fd860207475752f9b4ac4ba
El Matador <32282170+misterrodger@users.noreply.github.com>
2019-05-14 11:04:45 +0300

    Update app.js

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div><h1>React and Webpack/Babel setup</h1></div>

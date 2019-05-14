abba6881a1a37a20acd3adac4595ccad3c87ada7
mrfong <therealshadix@gmail.com>
2019-05-14 10:52:18 +0300

    removed redirect relates to #42

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -13,7 +13,7 @@ routers.post('/create-user', (req, res) => {
   // create new entry in database with parent's info
 
   // direct them to /parent-sign-in, and direct to /parent-profile
-  res.redirect('/parent-profile');
+  res.end();
 });
 
 routers.post('/create-user', (req, res) => {
@@ -22,38 +22,38 @@ routers.post('/create-user', (req, res) => {
   // create new entry in database with parent's info
 
   // direct them to /parent-sign-in, and direct to /parent-profile
-  res.redirect('/parent-profile');
+  res.end();
 });
 
 routers.get('/parent-profile', (req, res) => {
   // call the function that gets personal info and booked events from the parent's profile
 
-  res.redirect('/');
+  res.end();
 });
 
 routers.get('/parent-events', (req, res) => {
   // call the function that Get list of events in the parent's location from the database
 
 
-  res.redirect('/');
+  res.end();
 });
 
 routers.post('/book-event', (req, res) => {
   // Add event to parent's event list in database
 
-  res.redirect('/parent-profile');
+  res.end();
 });
 
 routers.get('/get-info', (req, res) => {
   // calls a function that gets child development info based on ages
   // gets it from a JSON file
 
-  res.redirect('/');
+  res.end();
 });
 
 routers.get('/get-suggestions', (req, res) => {
 // calls a function that gets suggested activities, filtered from events list by age and superpowers
-  res.redirect('/');
+  res.end();
 });
 
 

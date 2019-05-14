2c2cebade26d3460bd8d7b04116a3104cdc16598
faris114 <farisabutaya@gmail.com>
2019-05-14 12:33:53 +0300

    Route file staging for test purpose Relates #51

--- client/src/components/routes.js
+++ client/src/components/routes.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import SignupLogin from './landing-page/landing-page' ;
-import LandingPage from './landing-page/landing-page' ;
+
+import ParentProfile from './parentProfile/parentProfile'
+import Events from './events/events'
+
 function landingPage() {
   return <h2>Landing page </h2>;
 }
@@ -14,30 +17,37 @@ function login() {
   return <h2>Login</h2>;
 }
 
-function AppRouter() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Landing-Page</Link>
-            </li>
-            <li>
-              <Link to="/login/">login</Link>
-            </li>
-            <li>
-              <Link to="/signup/">Sign up</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Route path="/Landing-page/" exact component={SignupLogin} />
-        <Route path="/about/" component={signUp} />
-        <Route path="/users/" component={login} />
-      </div>
-    </Router>
-  );
-}
+// function AppRouter() {
+//   return (
+//     <Router>
+//       <div>
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/Landing-page">Landing-Page</Link>
+//             </li>
+//             <li>
+//               <Link to="/signup">login</Link>
+//             </li>
+//             <li>
+//               <Link to="/login/">Sign up</Link>
+//             </li>
+//           </ul>
+//         </nav>
+//
+//         <Route path="/Landing-page/" exact component={SignupLogin} />
+//
+//         <Route path="/parent-profile/" component={ParentProfile} />
+//         <Route path="/events/" component={Events} />
+//         <Route path="/parent-info/" component={ParentInfo} />
+//         <Route path="/events/" component={Events} />
+//         <Route path="/get-info/" component={GetInfo} />
+//         <Route path="/logout/" component={Logout} />
+//         <Route path="/get-suggestions/" component={Suggestions} />
+//
+//       </div>
+//     </Router>
+//   );
+// }
 
 export default AppRouter;

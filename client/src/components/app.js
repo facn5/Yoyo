340ef258e9a63d7591f963481205329da2bffe46
mrfong <therealshadix@gmail.com>
2019-05-14 17:52:28 +0300

    removed unused import related to #57

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import HeaderC from './header/index';
 import Navbar from './navbar/index';
 import SignupLogin from './landing-page/landing-page';

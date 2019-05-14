46845b32706fa7565a783330c34ff4aedec0b09c
mrfong <therealshadix@gmail.com>
2019-05-14 14:14:39 +0300

    added navbar, added css styling for navbar relates to #57

---
+++ client/src/components/navbar/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./index.css";
+
+export default class navbar extends React.Component {
+   render() {
+return (
+  <div className="navbar">
+
+  <button type="button">Profile</button>
+  <button type="button" > Events</button>
+  <button type="button" >Info</button>
+
+
+   </div>
+ );
+
+
+   }
+
+}

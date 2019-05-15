1fe9ffe64787638cb0f692a57293e7e1291da6a5
mrfong <therealshadix@gmail.com>
2019-05-15 14:09:50 +0300

    converted to functional component relates to #43

--- client/src/components/parentInfo/parentInfo.js
+++ client/src/components/parentInfo/parentInfo.js
@@ -3,15 +3,10 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import HeaderC from './header/index';
 import Navbar from './navbar/index';
 
-export default class ParentInfo extends React.Component {
-  render() {
-    return (
+export const ParentInfo = ()=>(
       <div>
       <HeaderC/>
      <Navbar/>
       <h1>Hello from parent Information</h1>
       </div>
-
-     )
-  }
-}
+    );

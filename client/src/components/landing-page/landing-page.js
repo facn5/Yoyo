be5890d20ab39e32b31790a998e4e1fec51950ab
faris114 <farisabutaya@gmail.com>
2019-05-15 15:26:29 +0300

    added routes to landing page tearted as a parent Relates 51#

--- client/src/components/landing-page/landing-page.js
+++ client/src/components/landing-page/landing-page.js
@@ -1,34 +1,41 @@
-import React from 'react' ;
+import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Login from '../login/login' ;
-import {ParentSignup} from '../parent-signup-form/parent-signup';
-import './landingPage.css';
+import Login from "../login/login";
+import { ParentSignup } from "../parent-signup-form/parent-signup";
+import "./landingPage.css";
+import Events from "../events/events";
 
 export default class SignupLogin extends React.Component {
-state = {landingPage: true};
-landingPageStatus= () =>{
-
-  this.setState({landingPage: false})
-}
+  state = { landingPage: true };
+  landingPageStatus = () => {
+    this.setState({ landingPage: false });
+  };
   render() {
     return (
       <div>
-<Router>
+        <Router>
+          <Link to="/signup">
+            <button
+              className={this.state.landingPage ? "show" : "hide"}
+              onClick={this.landingPageStatus}
+            >
+              Signup
+            </button>
+          </Link>
 
-        <Link  to="/signup"><button className={this.state.landingPage?"show":"hide"} onClick={this.landingPageStatus}>Signup</button></Link>
-
-        <Link to="/login/"><button className={this.state.landingPage?"show":"hide"} onClick={this.landingPageStatus}>Login</button></Link>
-
-      <Route path="/signup/" component={ParentSignup} />
-      <Route path="/login/" component={Login} />
-
-
-
-
-
-</Router>
+          <Link to="/login/">
+            <button
+              className={this.state.landingPage ? "show" : "hide"}
+              onClick={this.landingPageStatus}
+            >
+              Login
+            </button>
+          </Link>
+          <Route path="/event" component={Events} />
+          <Route path="/signup/" component={ParentSignup} />
+          <Route path="/login/" component={Login} />
+        </Router>
       </div>
-
-     )
+    );
   }
 }

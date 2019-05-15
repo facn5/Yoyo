cad1431e9acbb445edb862b8345f07ba22b5318f
faris114 <farisabutaya@gmail.com>
2019-05-15 12:48:52 +0300

    added state with function to hide/component Relates 51#

--- client/src/components/landing-page/landing-page.js
+++ client/src/components/landing-page/landing-page.js
@@ -5,14 +5,19 @@ import {ParentSignup} from '../parent-signup-form/parent-signup';
 import './landingPage.css';
 
 export default class SignupLogin extends React.Component {
+state = {landingPage: true};
+landingPageStatus= () =>{
+
+  this.setState({landingPage: false})
+}
   render() {
     return (
       <div>
 <Router>
 
-        <Link  to="/signup"><button>Signup</button></Link>
+        <Link  to="/signup"><button className={this.state.landingPage?"show":"hide"} onClick={this.landingPageStatus}>Signup</button></Link>
 
-        <Link to="/login/"><button>Login</button></Link>
+        <Link to="/login/"><button className={this.state.landingPage?"show":"hide"} onClick={this.landingPageStatus}>Login</button></Link>
 
       <Route path="/signup/" component={ParentSignup} />
       <Route path="/login/" component={Login} />

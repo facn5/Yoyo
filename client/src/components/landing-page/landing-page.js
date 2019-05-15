aa1a29be0090a8c37fd3bb6c8f96c2fae483ccff
mrfong <therealshadix@gmail.com>
2019-05-15 12:21:12 +0300

    removed decoration from the link underline relates to #43

--- client/src/components/landing-page/landing-page.js
+++ client/src/components/landing-page/landing-page.js
@@ -10,10 +10,13 @@ export default class SignupLogin extends React.Component {
       <div>
 <Router>
 
-        <Link  to="/signup"><button>Signup</button></Link>
-
-        <Link to="/login/"><button>Login</button></Link>
-
+<div className="main-container">
+<div className="image">
+<img src="https://user-images.githubusercontent.com/32282170/57733684-01464600-7698-11e9-89e7-b9debad0bee0.png"/>
+</div>
+        <Link to="/login/"style={{ textDecoration: 'none' }}><button className="login">Login</button></Link>
+        <Link  to="/signup" style={{ textDecoration: 'none' }}><button className="signup">Signup</button></Link>
+</div>
       <Route path="/signup/" component={ParentSignup} />
       <Route path="/login/" component={Login} />
 

82514a328f1fb34dca7fd044272e72cec52da9f7
mrfong <therealshadix@gmail.com>
2019-05-15 12:22:10 +0300

    added header,navbar, spaced out text relates to #43

--- client/src/components/parentInfo/parentInfo.js
+++ client/src/components/parentInfo/parentInfo.js
@@ -1,11 +1,15 @@
 import React from 'react' ;
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import HeaderC from './header/index';
+import Navbar from './navbar/index';
+
 export default class ParentInfo extends React.Component {
   render() {
     return (
       <div>
-      
-      <h1>Hello from parentInformation</h1>
+      <HeaderC/>
+     <Navbar/>
+      <h1>Hello from parent Information</h1>
       </div>
 
      )

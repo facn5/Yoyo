65fe6b353e385d1306316bc0a819d60247819703
misterrodger <johndavidrodger@gmail.com>
2019-05-16 13:15:00 +0300

    Split routes.js into separate filds
    -Separate files for each router: parent, event, teacher
    -Relates #41
    -

--- server/routes.js
+++ server/routes.js
@@ -1,50 +1,59 @@
-const express = require('express');
-const router = express.Router();
-const Parent = require('./database/schemas/Parent.js')
-
-router.get('/', async (req, res) => {
-  try {
-    const parent = await Parent.find();
-    res.json(parent)
-  } catch (err) {
-    res.json({
-      message: err
-    });
-  }
-});
-
-router.post('/', async (req, res) => {
-  const parent = new Parent({
-    name: req.body.name,
-    password: req.body.password,
-    phone: req.body.phone,
-    email: req.body.email,
-    arabic: req.body.arabic,
-    english: req.body.english,
-    hebrew: req.body.hebrew,
-    russian: req.body.russian,
-    birthTo1: req.body.birthTo1,
-    oneToTwo: req.body.oneToTwo,
-    twoToFour: req.body.twoToFour,
-    fiveToSix: req.body.fiveToSix,
-    sevenToEight: req.body.sevenToEight,
-    nineToTen: req.body.nineToTen,
-    sports: req.body.sports,
-    art: req.body.art,
-    theatre: req.body.theatre,
-    computers: req.body.computers,
-    music: req.body.music,
-    social: req.body.social,
-  });
-  try{
-    const savedParent = await parent.save()
-    res.json(savedParent);
-  } catch(err) {
-    res.json({ message: err });
-  }
-});
-
-module.exports = router;
+// const express = require('express');
+// const router = express.Router();
+// const Parent = require('./database/schemas/Parent.js')
+//
+// router.get('/', async (req, res) => {
+//   try {
+//     const parent = await Parent.find();
+//     res.json(parent)
+//   } catch (err) {
+//     res.json({
+//       message: err
+//     });
+//   }
+// });
+//
+// router.post('/', async (req, res) => {
+//   const parent = new Parent({
+//     name: req.body.name,
+//     password: req.body.password,
+//     phone: req.body.phone,
+//     email: req.body.email,
+//     arabic: req.body.arabic,
+//     english: req.body.english,
+//     hebrew: req.body.hebrew,
+//     russian: req.body.russian,
+//     birthTo1: req.body.birthTo1,
+//     oneToTwo: req.body.oneToTwo,
+//     twoToFour: req.body.twoToFour,
+//     fiveToSix: req.body.fiveToSix,
+//     sevenToEight: req.body.sevenToEight,
+//     nineToTen: req.body.nineToTen,
+//     sports: req.body.sports,
+//     art: req.body.art,
+//     theatre: req.body.theatre,
+//     computers: req.body.computers,
+//     music: req.body.music,
+//     social: req.body.social,
+//   });
+//   try{
+//     const savedParent = await parent.save()
+//     res.json(savedParent);
+//   } catch(err) {
+//     res.json({ message: err });
+//   }
+// });
+//
+// router.get('/:parentID', async (req, res) => {
+//   try{
+//     const profile = await Parent.findById(req.params.parentID);
+//     res.json(profile);
+//   } catch (err) {
+//     res.json({ message: err });
+//   }
+// });
+//
+// module.exports = router;
 
 // router.get("/getData", (req, res) => {
 //   Data.find((err, data) => {

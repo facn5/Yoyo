73adb850cde89bcb52e5d790f5276f695bd89776
mrfong <therealshadix@gmail.com>
2019-05-16 14:49:27 +0300

    added a state, structure, data for parentProfile page relates to #72

--- client/src/components/parentProfile/parentProfile.js
+++ client/src/components/parentProfile/parentProfile.js
@@ -1,10 +1,33 @@
 import React from 'react' ;
+import HeaderC from '../header/index';
+import Navbar from "../navbar/index";
+import StyledButton from "../button/index";
+import './profile.css';
+
 export default class ParentProfile extends React.Component {
+  state={
+    name:"Shadi",
+    birth:"1 january 2008",
+    language:"Arabic",
+    superpower:"Swimming",
+    specialneed:"Speech Therapy",
+    curevents:"Coding Bootcamp,Flying"
+  };
   render() {
     return (
       <div>
-      <h1>Hello from parentProfile</h1>
-
+      <HeaderC/>
+            <h1>Parent's Desk</h1>
+            <Navbar/>
+      <div className="Card-Info">
+    <pre>Name:{this.state.name}</pre>
+    <pre>Birth Date:{this.state.birth}</pre>
+    <pre>Languages:{this.state.language}</pre>
+    <pre>Superpowers:{this.state.superpower}</pre>
+    <pre>Special Needs:{this.state.specialneed}</pre>
+    <pre>Current Events:{this.state.curevents}</pre>
+    <button>Logout</button>
+      </div>
       </div>
 
      )

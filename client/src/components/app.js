7124c2f166c300bd4ab1b7fbcef98fdca1d2e3d9
mrfong <therealshadix@gmail.com>
2019-05-16 14:45:10 +0300

    removed,added things from app.jks relates to #72

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch, browserHistory  } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import SignupLogin from "./landing-page/landing-page";
 import {ParentSignup}  from "./parent-signup-form/parent-signup";
+import ParentProfile  from "./parentProfile/parentProfile";
 import Login from "./login/login";
 import Events from "./events/events";
 
 class App extends Component {
   render() {
     return (
-      <Router history={browserHistory}>
+      <Router >
       <Switch>
         <Route path='/' exact component={SignupLogin} />
-        <Route path="/event" exact component={Events} />
+        <Route path="/events" exact component={Events} />
         <Route path="/signup" exact component={ParentSignup} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/profile" component={ParentProfile} />
+
         </Switch>
       </Router>
     );

7aff1752a9cd02f4cc59118ca4b3a16d458ab172
misterrodger <johndavidrodger@gmail.com>
2019-05-16 14:52:08 +0300

    Remove unused router from app.js and deleted unused folders/files
    -Relates#41
    -
    -

--- server/app.js
+++ server/app.js
@@ -1,11 +1,10 @@
-const mongoose = require("mongoose");
-const express = require("express");
+const mongoose = require('mongoose');
+const express = require('express');
 const cors = require('cors');
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-// const routes = require('./routes.js')
-const parentRoute = require('./controllers/parent/routes.js')
-const eventRoute = require('./controllers/event/routes.js')
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const parentRoute = require('./controllers/parent/routes.js');
+const eventRoute = require('./controllers/event/routes.js');
 const teacherRoute = require('./controllers/teacher/routes.js')
 require('env2')('../config.env');
 
@@ -13,21 +12,20 @@ let DB_URL = process.env.DB_URL;
 const API_PORT = 3001;
 const app = express();
 app.use(cors());
-const router = express.Router();
 
 mongoose.connect(DB_URL, { useNewUrlParser: true })
   .then(() => console.log('Database connected successfully'))
   .catch(err => console.log(err));
 
-let db = mongoose.connection;
+const db = mongoose.connection;
 
-db.once("open", () => console.log("connected to the database"));
+db.once("open", () => console.log('connected to the database'));
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(logger("dev"));
-// app.use("/api", router);
+app.use(logger('dev'));
+
 app.use('/parent', parentRoute);
 app.use('/event', eventRoute);
 app.use('/teacher', teacherRoute);

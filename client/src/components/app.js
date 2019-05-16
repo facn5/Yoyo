8a752083341dc51230da18a8ea200f6e3fce877e
faris114 <farisabutaya@gmail.com>
2019-05-16 15:34:34 +0300

    added profile and info routes Relates #51

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -4,6 +4,8 @@ import SignupLogin from "./landing-page/landing-page";
 import {ParentSignup}  from "./parent-signup-form/parent-signup";
 import Login from "./login/login";
 import Events from "./events/events";
+import ParentProfile from './parentProfile/parentProfile'
+import {ParentInfo}  from './parentInfo/parentInfo'
 
 class App extends Component {
   render() {
@@ -13,6 +15,8 @@ class App extends Component {
         <Route path="/event" exact component={Events} />
         <Route path="/signup" exact component={ParentSignup} />
         <Route path="/login" exact component={Login} />
+        <Route path="/profile" exact component={ParentProfile} />
+        <Route path="/info" exact component={ParentInfo} />
       </Router>
     );
   }

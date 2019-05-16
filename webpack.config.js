a01ef3508738debb223b7ab4a86ea40393e76292
faris114 <farisabutaya@gmail.com>
2019-05-16 12:21:32 +0300

    solved problem of not getting to pages on refresh or inserting path in the address bar Relates 51#

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './client/src/index.js',
-  output: { path: path.join(__dirname, '/dist'), filename: 'bundle.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/',
+  },
   module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: 'babel-loader',
-    },
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-    }],
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   plugins: [new HtmlWebpackPlugin({ template: './client/src/index.html' })],
 };

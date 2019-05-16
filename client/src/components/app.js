f70a515c8681c036647a7bb07867b01c2112712b
Faris Abutaya <45868675+faris114@users.noreply.github.com>
2019-05-16 15:58:26 +0300

    Deleted unused import

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch, } from "react-router-dom";
+import { BrowserRouter as Router, Route, } from "react-router-dom";
 import SignupLogin from "./landing-page/landing-page";
 import {ParentSignup}  from "./parent-signup-form/parent-signup";
 import Login from "./login/login";

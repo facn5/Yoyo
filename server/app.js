cd6c1be3573982ec6d5069db33165d0b6643f182
misterrodger <johndavidrodger@gmail.com>
2019-05-16 13:13:23 +0300

    Add app.js file
    -changed name from server.js to app.js
    -added new routes to /parent, /teacher, and /event
    -Relates #41

--- server/app.js
+++ server/app.js
@@ -1,16 +1,35 @@
-const express = require('express');
-const bodyparser = require('body-parser');
-const compression = require('compression');
-const controllers = require('./controllers/routes');
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+// const routes = require('./routes.js')
+const parentRoute = require('./controllers/parent/routes.js')
+const eventRoute = require('./controllers/event/routes.js')
+const teacherRoute = require('./controllers/teacher/routes.js')
+require('env2')('../config.env');
 
+let DB_URL = process.env.DB_URL;
+const API_PORT = 3001;
 const app = express();
+app.use(cors());
+const router = express.Router();
 
-app.disable('x-powered-by');
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(compression());
-app.use(controllers);
-app.set('port', process.env.PORT || 7777);
+mongoose.connect(DB_URL, { useNewUrlParser: true })
+  .then(() => console.log('Database connected successfully'))
+  .catch(err => console.log(err));
 
-module.exports = app;
+let db = mongoose.connection;
+
+db.once("open", () => console.log("connected to the database"));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger("dev"));
+// app.use("/api", router);
+app.use('/parent', parentRoute);
+app.use('/event', eventRoute);
+app.use('/teacher', teacherRoute);
+
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));

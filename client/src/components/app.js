32a8b38241324ec7b6a172b862dfb5f5b174266d
faris114 <farisabutaya@gmail.com>
2019-05-19 10:47:17 +0300

    deleted unused routes #51

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -4,8 +4,7 @@ import SignupLogin from "./landing-page/landing-page";
 import {ParentSignup}  from "./parent-signup-form/parent-signup";
 import Login from "./login/login";
 import Events from "./events/events";
-import ParentProfile from './parentProfile/parentProfile'
-import {ParentInfo}  from './parentInfo/parentInfo'
+
 
 class App extends Component {
   render() {
@@ -15,8 +14,7 @@ class App extends Component {
         <Route path="/event" exact component={Events} />
         <Route path="/signup" exact component={ParentSignup} />
         <Route path="/login" exact component={Login} />
-        <Route path="/profile" exact component={ParentProfile} />
-        <Route path="/info" exact component={ParentInfo} />
+
       </Router>
     );
   }

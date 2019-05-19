0ae0c02b492b585042b17c06447ab3daf58eec72
faris114 <farisabutaya@gmail.com>
2019-05-19 14:49:29 +0300

    added navbar header and basic button for test purpose Relates #72

--- client/src/components/events/events.js
+++ client/src/components/events/events.js
@@ -1,23 +1,25 @@
 import React from 'react' ;
-import CardComponent from '../cardComponent/cardComponent'
+import EventCard from './eventsCardComponent'
+import HeaderC from '../header/index';
+import Navbar from '../navbar/index';
+import './eventsCardComponent.css' ;
 
 export default class Events extends React.Component {
+
 
   render() {
     return (
       <div>
       <h1>Hello from events</h1>
-      <CardComponent name="name" />
-      <CardComponent name="location" />
-      <CardComponent name="description" />
-      <CardComponent name="photo" />
-      <CardComponent name="days" />
-      <CardComponent name="singlePrice" />
-      <CardComponent name="termPrice" />
-      <CardComponent name="maxChild" />
-      <CardComponent name="ages" />
-      <CardComponent name="superpowers" />
-      <CardComponent name="languages" />
+      <HeaderC />
+      <Navbar />
+      <div className="titles">
+      <h1>time</h1>
+      <h1>name</h1>
+      <h1>ages</h1>
+      </div>
+      <EventCard name="name" ages="ages" time="time" />
+
 
       </div>
 

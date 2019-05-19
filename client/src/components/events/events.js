aa017fc383d5d619c9a82be113e03a52d1a2f983
mrfong <therealshadix@gmail.com>
2019-05-19 12:19:03 +0300

    fixed a problem with css import, done with making the events card react component relates to #72

--- client/src/components/events/events.js
+++ client/src/components/events/events.js
@@ -1,8 +1,8 @@
 import React from 'react' ;
 import HeaderC from '../header/index';
 import Navbar from '../navbar/index';
-
-import './events.css'
+import './events.css';
+import EventCard from '../event-card/card';
 
 export default class Events extends React.Component {
 
@@ -17,45 +17,9 @@ export default class Events extends React.Component {
    <button className="next">This Week:12-19 May</button>   <button>Next Week:19-26May </button>
    </div>
    <br/>
-   <div className="Event-Card">
-   <p>Sunday, 13th of May</p>
-   <div className="three-columns">
-    <p>2-4pm</p>
-    <p>Peanuts Football</p>
-    <p>age 6-10</p>
-   </div>
-   <div className="three-columns">
-    <p>6-10pm</p>
-    <p>Ammo Roni</p>
-    <p>age 4-10</p>
-   </div>
-   </div>
-   <div className="Event-Card">
-   <p>Sunday, 14th of May</p>
-   <div className="three-columns">
-    <p>2-4pm</p>
-    <p>Peanuts Football</p>
-    <p>age 6-10</p>
-   </div>
-   <div className="three-columns">
-    <p>6-10pm</p>
-    <p>Ammo Roni</p>
-    <p>age 4-10</p>
-   </div>
-   </div>
-   <div className="Event-Card">
-   <p>Sunday, 15th of May</p>
-   <div className="three-columns">
-    <p>2-4pm</p>
-    <p>Peanuts Football</p>
-    <p>age 6-10</p>
-   </div>
-   <div className="three-columns">
-    <p>6-10pm</p>
-    <p>Ammo Roni</p>
-    <p>age 4-10</p>
-   </div>
-   </div>
+  <EventCard/>
+   // <EventCard/>
+   // <EventCard/>
    <img className="search" src="https://i.imgur.com/9PAikvm.png"/>
 
       </div>

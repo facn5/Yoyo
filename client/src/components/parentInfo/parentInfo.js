bfb561c8ec7e95fa3cedf635420c665de8ca9ceb
mrfong <therealshadix@gmail.com>
2019-05-19 12:43:00 +0300

    added parentInfo page relates to #72

--- client/src/components/parentInfo/parentInfo.js
+++ client/src/components/parentInfo/parentInfo.js
@@ -1,12 +1,38 @@
 import React from 'react' ;
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import HeaderC from './header/index';
-import Navbar from './navbar/index';
+import HeaderC from '../header/index';
+import Navbar from '../navbar/index';
+import './info.css';
 
-export const ParentInfo = ()=>(
+export default class ParentInfo extends React.Component {
+state = {
+name:"Fadi",
+age:"One",
+suggestion:"Baby Dabke",
+};
+  render() {
+    return (
+      <div>
       <div>
       <HeaderC/>
-     <Navbar/>
-      <h1>Hello from parent Information</h1>
-      </div>
-    );
+            <h1>Parent's Desk</h1>
+            <Navbar/>
+            <button className="age-button">This Age </button>
+            <div className="Info">
+            <p className="age-info">{this.state.name} is {this.state.age} years old and is learning to understand sequences of events.  {this.state.name} will watch you do everything from drinking a cup of tea to doing the laundry.  Explain the sequence of events to {this.state.name} as you are doing things.  Games and activities that can help {this.state.name} development during this period include:
+
+Singing and music with movement
+Conversation
+Household chores
+Personal care
+Drawing</p>
+<button>Suggestions</button>
+<p className="suggestion" >
+{this.state.suggestion}
+</p>
+            </div>
+            </div>
+    </div>
+     )
+  }
+}

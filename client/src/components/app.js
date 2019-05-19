1e6d9efdd86fc7854373ff25864a9cdf30ee5a29
faris114 <farisabutaya@gmail.com>
2019-05-19 14:47:03 +0300

    added routes to all components Relates 51#

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import SignupLogin from "./landing-page/landing-page";
-import {ParentSignup}  from "./parent-signup-form/parent-signup";
+import { ParentSignup } from "./parent-signup-form/parent-signup";
 import Login from "./login/login";
 import Events from "./events/events";
-
+import ParentProfile from './parentProfile/parentProfile'
+import ParentInfo from './parentInfo/parentInfo'
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route path='/' exact component={SignupLogin} />
-        <Route path="/event" exact component={Events} />
-        <Route path="/signup" exact component={ParentSignup} />
-        <Route path="/login" exact component={Login} />
-
+        <Route path="/" exact component={SignupLogin} />
+        <Route path="/events/" exact component={Events} />
+        <Route path="/signup/" exact component={ParentSignup} />
+        <Route path="/login/" exact component={Login} />
+        <Route path="/profile/" exact component={ParentProfile} />
+        <Route path="/parentinfo" exact component={ParentInfo} />
       </Router>
     );
   }

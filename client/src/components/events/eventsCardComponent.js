e05a18a2458c750b785aa334fc3b9632813419c0
faris114 <farisabutaya@gmail.com>
2019-05-19 15:02:20 +0300

    added events card component Relates #72

---
+++ client/src/components/events/eventsCardComponent.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './eventsCardComponent.css';
+
+export default class EventCard extends React.Component {
+
+  render() {
+    return (
+      <div>
+
+            <button type="button" className="btn">
+
+            <div className="time">{this.props.time}</div>
+            <div className="name">{this.props.name}</div>
+            <div className="ages">{this.props.ages}</div>
+
+           </button>
+
+      </div>
+    );
+  }
+}

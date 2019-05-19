e49ad77f8173ac3ff63c871a1f31eabccce1d2fe
faris114 <farisabutaya@gmail.com>
2019-05-19 10:41:07 +0300

    Created events card as button component Relates #73

--- client/src/components/events/events.js
+++ client/src/components/events/events.js
@@ -1,10 +1,24 @@
 import React from 'react' ;
+import CardComponent from '../cardComponent/cardComponent'
 
 export default class Events extends React.Component {
+
   render() {
     return (
       <div>
       <h1>Hello from events</h1>
+      <CardComponent name="name" />
+      <CardComponent name="location" />
+      <CardComponent name="description" />
+      <CardComponent name="photo" />
+      <CardComponent name="days" />
+      <CardComponent name="singlePrice" />
+      <CardComponent name="termPrice" />
+      <CardComponent name="maxChild" />
+      <CardComponent name="ages" />
+      <CardComponent name="superpowers" />
+      <CardComponent name="languages" />
+
       </div>
 
      )

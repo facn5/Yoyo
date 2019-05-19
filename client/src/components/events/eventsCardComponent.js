36e10577a6e63036a0da3ab5b9f7dc801cb8fcae
Faris Abutaya <45868675+faris114@users.noreply.github.com>
2019-05-19 15:48:25 +0300

    converted to functional component

--- client/src/components/events/eventsCardComponent.js
+++ client/src/components/events/eventsCardComponent.js
@@ -1,13 +1,12 @@
 import React from "react";
-EventCard = () =>  
-
-
-    return (<div>
-            <button type="button" className="btn">
-            <div className="time">{this.props.time}</div>
-            <div className="name">{this.props.name}</div>
-            <div className="ages">{this.props.ages}</div>
-           </button>
-      </div>
-    );
-export EventCard
+export const EventCard = props => {
+  return (
+    <div>
+      <button type="button" className="btn">
+        <div className="time">{props.time}</div>
+        <div className="name">{props.name}</div>
+        <div className="ages">{props.ages}</div>
+      </button>
+    </div>
+  );
+};

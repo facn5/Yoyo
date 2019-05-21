c604e46836fe557da4f09a98970fcbbfc336863b
mrfong <therealshadix@gmail.com>
2019-05-21 14:09:28 +0300

    changed the state into props relates to #72

--- client/src/components/events/eventsCardComponent.js
+++ client/src/components/events/eventsCardComponent.js
@@ -2,20 +2,14 @@ import React from 'react' ;
 import './eventsCardComponent.css'
 
 export default class Events extends React.Component {
-  state = {
-    day:"Sunday, 13th of May",
-    name:"Peanuts Football",
-    time:"2pm-4pm",
-    age:"6-10",
-  };
   render() {
     return (
   <div className="event-card">
-  <p>{this.state.day}</p>
+  <p>{this.props.day}</p>
   <button className="three-columns">
-  <pre>{this.state.time}</pre>
-   <pre>{this.state.name}</pre>
-  <pre>{this.state.age}</pre>
+  <pre>{this.props.time}</pre>
+   <pre>{this.props.name}</pre>
+  <pre>{this.props.age}</pre>
   </button>
 
 

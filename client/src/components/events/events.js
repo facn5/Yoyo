a4c6b7b8cb2da0de6e734ccb36eb471bceb4b14d
mrfong <therealshadix@gmail.com>
2019-05-21 12:43:15 +0300

    edited the event page relates to #72

--- client/src/components/events/events.js
+++ client/src/components/events/events.js
@@ -1,9 +1,8 @@
 import React from 'react' ;
-import {EventCard} from './eventsCardComponent'
 import HeaderC from '../header/index';
 import Navbar from '../navbar/index';
-import './eventsCardComponent.css' ;
-
+import './events.css';
+import EventCard from './eventsCardComponent';
 
 export default class Events extends React.Component {
 
@@ -11,19 +10,17 @@ export default class Events extends React.Component {
   render() {
     return (
       <div>
+      <HeaderC/>
+    <h1>Events</h1>
+    <Navbar/>
+   <div className="twobuttons">
+   <button className="next">This Week:12-19 May</button>   <button>Next Week:19-26May </button>
+   </div>
+   <br/>
+  <EventCard/>
+  <EventCard/>
+  <EventCard/>
    <img className="search" src="https://i.imgur.com/9PAikvm.png"/>
-
-      <h1>Hello from events</h1>
-      <HeaderC />
-      <Navbar />
-      <div className="titles">
-      <h1>time</h1>
-      <h1>name</h1>
-      <h1>ages</h1>
-      </div>
-      <EventCard name="name" ages="ages" time="time" />
-
-
 
       </div>
 

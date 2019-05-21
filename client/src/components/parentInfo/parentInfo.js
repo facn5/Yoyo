454a073e1c57b8c2c05e4d709a3ec4633273db97
Shadi RR <therealshadix@gmail.com>
2019-05-21 10:50:17 +0300

    Update parentInfo.js

--- client/src/components/parentInfo/parentInfo.js
+++ client/src/components/parentInfo/parentInfo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import HeaderC from '../header/index';
+import Navbar from '../navbar/index';
+import './info.css';
 
 export default class ParentInfo extends React.Component {
 state = {
@@ -15,7 +17,7 @@ suggestion:"Baby Dabke",
       <HeaderC/>
             <h1>Parent's Desk</h1>
             <Navbar/>
-            <button className="age-button">This Age </button>
+            <h1>This Age </h1>
             <div className="Info">
             <p className="age-info">{this.state.name} is {this.state.age} years old and is learning to understand sequences of events.  {this.state.name} will watch you do everything from drinking a cup of tea to doing the laundry.  Explain the sequence of events to {this.state.name} as you are doing things.  Games and activities that can help {this.state.name} development during this period include:
 
@@ -24,7 +26,7 @@ Conversation
 Household chores
 Personal care
 Drawing</p>
-<button>Suggestions</button>
+<h1>Suggestions</h1>
 <p className="suggestion" >
 {this.state.suggestion}
 </p>

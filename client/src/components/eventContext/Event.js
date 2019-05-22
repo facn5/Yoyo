3ea5f3b4ec4019c101fa3539fc570bd40bd78371
misterrodger <johndavidrodger@gmail.com>
2019-05-22 22:23:48 +0300

    Add html meta tags
    -Give info to search engines and screen readers
    -Relates #103
    -

--- client/src/components/eventContext/Event.js
+++ client/src/components/eventContext/Event.js
@@ -3,12 +3,14 @@ import React from 'react';
 const Event = (props) => {
   return(
     <div>
-      <h2>{props.name} for age {props.minAge} to {props.maxAge}</h2>
+      <fieldset>
+      <legend><h2>{props.name} for age {props.minAge} to {props.maxAge}</h2></legend>
       <h3>{props.days} in {props.location}</h3>
       <p>{props.description}</p>
       <p>Single price:{props.singlePrice}, Full term:{props.termPrice}</p>
       <p>Languages:{props.languages} Superpowers:{props.superpowers}</p>
       {props.photo}
+      </fieldset>
     </div>
   )
 }

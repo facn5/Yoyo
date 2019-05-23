0a9a2a097ffce207a1465cbd87244a78b98f6e93
misterrodger <johndavidrodger@gmail.com>
2019-05-23 09:21:16 +0300

    Add toggle feature to Events using React useState
    -On load, events only display Name, day and location
    -onClick the field expands to display the other info
    -Tested and working
    Relates #25

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import SignupLogin from "./landing-page/landing-page";
 import ParentSignup  from "./parent-signup-form/parent-signup";
